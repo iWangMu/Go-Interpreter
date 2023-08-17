a6db2016814a41fbf28f5d56431e532c63be7cf1
wangmu <wangmu8889@gmail.com>
2023-08-17 19:46:13 +0800

    repl

---
+++ repl/repl.go
@@ -0,0 +1,29 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/iWangMu/Go-Interpreter/lexer"
+	"github.com/iWangMu/Go-Interpreter/token"
+	"io"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+		lex := lexer.New(line)
+
+		for tok := lex.NextToken(); tok.Type != token.EOF; tok = lex.NextToken() {
+			fmt.Fprintf(out, "%+v\n", tok)
+		}
+	}
+}
